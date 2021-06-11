243adc240899f0689e162da51fcf4943ab0bef59
Sajjad Ahmadi <sajjad.ahmadi.sh>
2021-06-11 09:39:00 +0430

    init

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+echo $SCRIPTPATH
+
+g++ -std=c++11 /utils/render_balls_so.cpp -o /utils/render_balls_so.so -shared -fPIC -O2 -D_GLIBCXX_USE_CXX11_ABI=0
