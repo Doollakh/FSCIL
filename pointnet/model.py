a49effe9cd43abed3847e1321dfaf3117b9be386
Sajjad Ahmadi <sajjad.ahmadi.sh@gmail.com>
2022-04-26 16:21:44 +0430

    make input_transform optional

--- pointnet/model.py
+++ pointnet/model.py
@@ -88,9 +88,11 @@ class STNkd(nn.Module):
 
 
 class PointNetfeat(nn.Module):
-    def __init__(self, global_feat=True, feature_transform=False):
+    def __init__(self, global_feat=True, feature_transform=False, input_transform=True):
         super(PointNetfeat, self).__init__()
-        self.stn = STN3d()
+        self.input_transform = input_transform
+        if self.input_transform:
+            self.stn = STN3d()
         self.conv1 = torch.nn.Conv1d(3, 64, 1)
         self.conv2 = torch.nn.Conv1d(64, 128, 1)
         self.conv3 = torch.nn.Conv1d(128, 1024, 1)
@@ -109,19 +111,20 @@ class PointNetfeat(nn.Module):
 
     def forward(self, x):
         n_pts = x.size()[2]
-        trans = self.stn(x)
-        x = x.transpose(2, 1)
-        x = torch.bmm(x, trans)
-        x = x.transpose(2, 1)
+        trans = None
+        if self.input_transform:
+            trans = self.stn(x)
+            x = x.transpose(2, 1)
+            x = torch.bmm(x, trans)
+            x = x.transpose(2, 1)
         x = F.relu(self.bn1(self.conv1(x)))
 
+        trans_feat = None
         if self.feature_transform:
             trans_feat = self.fstn(x)
             x = x.transpose(2, 1)
             x = torch.bmm(x, trans_feat)
             x = x.transpose(2, 1)
-        else:
-            trans_feat = None
 
         pointfeat = x
         x = F.relu(self.bn2(self.conv2(x)))
