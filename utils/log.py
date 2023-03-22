e9f0e66856a8341cc2addf55b3331cf31c58dffb
Reza Tanakizadeh <reza.tz780210@gmail.com>
2023-03-22 16:47:08 +0430

    (REFACTOR)

--- utils/log.py
+++ utils/log.py
@@ -16,3 +16,21 @@ class Log():
     # Add data from Opt
     def add_opt(self):
         self.data['config']['batchSize'] = self.opt.batchSize
+        self.data['config']['num_points'] = self.opt.num_points
+        self.data['config']['workers'] = self.opt.workers
+        self.data['config']['learning_type'] = self.opt.learning_type
+        self.data['config']['loss_type'] = self.opt.loss_type
+        self.data['config']['start_num_class'] = self.opt.start_num_class
+        self.data['config']['step_num_class'] = self.opt.step_num_class
+        self.data['config']['n_cands'] = self.opt.n_cands
+        self.data['config']['dist_temperature'] = self.opt.dist_temperature
+        self.data['config']['dist_factor'] = self.opt.dist_factor
+        self.data['config']['cands_path'] = self.opt.cands_path
+        self.data['config']['few_shots'] = self.opt.few_shots
+        self.data['config']['model'] = self.opt.model
+        self.data['config']['dataset'] = self.opt.dataset
+        self.data['config']['dataset_type'] = self.opt.dataset_type
+        self.data['config']['feature_transform'] = self.opt.feature_transform
+        self.data['config']['input_transform'] = self.opt.input_transform
+        self.data['config']['KD'] = self.opt.KD
+
