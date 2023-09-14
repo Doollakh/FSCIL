f09893c0d1b1ddebc8b714af81da48a0e4306a03
Reza Tanakizadeh <reza.tz780210@gmail.com>
2023-09-14 08:40:31 +0000

    (FEAT) Used modified version of spectral clustring

--- pointnet/bests.py
+++ pointnet/bests.py
@@ -16,6 +16,7 @@ import numpy as np
 import scipy.spatial
 import scipy
 
+from spectural import SpectralClusteringMod
 cos = lambda in1, in2: np.dot(in1, in2) / (np.linalg.norm(in1) * np.linalg.norm(in2))
 
 
@@ -163,4 +164,82 @@ def spectral_clustring(dataset, classifier, n_class, classes_name, save_path, st
         o3d.io.write_point_cloud(os.path.join(save_path,f"{classes_name[i]}_{j}.ply"), pcd)
 
 
+
+def spectral_clustring_mod(dataset, classifier, n_class, classes_name, save_path, stage_id, start_class):
+  dataloader = torch.utils.data.DataLoader(
+                  dataset,
+                  batch_size=32,
+                  shuffle=False,
+                  num_workers=int(2))
+
+  print('computing best samples...')
+
+  result = {i:[] for i in range(n_class)}
+  which_classes = range(n_class) if stage_id == 1 else range(start_class,n_class)
+
+  for class_idx in which_classes:
+    print(f'class {class_idx}:{classes_name[class_idx]}')
+    X = np.array([])
+    y = np.array([])
+    P = np.array([])
+    for data in dataloader:
+        points, target = data
+        points = points.transpose(2, 1)
+        points, target = points.cuda(), target.cuda()
+        target = target.cpu().detach().numpy()
+
+        ##### checking and selecting based on each classe
+        which = target == np.array([class_idx for _ in range(len(target))])
+        if sum(which) != 0:
+          target = target[which]
+          points = points[which]
+
+          classifier = classifier.eval()
+          ـ , ـ, ـ, pred = classifier.feat(points)
+
+          X = np.concatenate((X, pred.detach().cpu().numpy()), 0) if len(X) != 0 else pred.cpu().detach().numpy()
+          y = np.concatenate((y, target), 0) if len(y) != 0 else target
+          P = np.concatenate((P, points.permute((0,2,1)).detach().cpu().numpy()), 0) if len(P) != 0 else points.permute((0,2,1)).cpu().detach().numpy()
+        
+          # break
+
+
+    dis_matrix = np.zeros((len(X),len(X)))
+    affinity_matrix = np.zeros((len(X),len(X)))
+
+    for b in range(len(X)):
+        for c in range(len(X)):
+          dis_matrix[b,c] =  chamfer_distance_modified(P[b], P[c], X[b], X[c])
+    
+    sigma = int(np.median(dis_matrix[dis_matrix != 0]))
+
+    for b in range(len(X)):
+        for c in range(len(X)):
+          distance = chamfer_distance_modified(P[b], P[c], X[b], X[c])
+          affinity_matrix[b,c] =  np.exp(-distance**2 / (2.0 * sigma**2))
+
+    # break
+    clustering = SpectralClusteringMod(n_clusters=5, random_state=0,affinity='precomputed').fit(affinity_matrix)
+    
+    # select groups
+    for c_label in range(np.max(clustering.labels_)+1):
+      selected_X = X[clustering.labels_ == c_label]
+      selected_w = y[clustering.labels_ == c_label]
+      n = len(selected_X)
+      r = np.zeros(n)
+      avg = np.average(selected_X, axis=0)
+      for j, item in enumerate(selected_X):
+          r[j] = cos(avg, item)
+      args = np.argsort(r)[::-1][:n]
+      result[class_idx].append(selected_w[args[0]])
+
+    # Save samples
+    pcd = o3d.geometry.PointCloud()
+
+    # Save as best exampler
+    for i in range(n_class):
+      id_list_of_top = result[i]
+      for j in range(len(id_list_of_top)):
+        pcd.points = o3d.utility.Vector3dVector(dataset[id_list_of_top[j]][0])
+        o3d.io.write_point_cloud(os.path.join(save_path,f"{classes_name[i]}_{j}.ply"), pcd)
 
--- train_classification.py
+++ train_classification.py
@@ -22,7 +22,7 @@ from tqdm import tqdm
 from pointnet.dataset import ModelNetDataset, ModelNet40, ScanObjects, ModelNet40_ScanObjects
 from pointnet.losses import KnowlegeDistilation, PointNetLoss
 from pointnet.model import PointNetCls, PointNetLwf
-from pointnet.bests import simple_clustring, spectral_clustring
+from pointnet.bests import simple_clustring, spectral_clustring, spectral_clustring_mod
 # learning_type = simple, joint, exemplar, lwf, bExemplar
 # forgetting
 from utils.general import increment_path
@@ -231,7 +231,7 @@ class Learning:
             if self.opt.best_type == 'simple':
                 simple_clustring(temp_dataset, temp_classifier, 40, self.class_names, best_save_path, stage_id, prv_n_class-self.opt.step_num_class)
             elif self.opt.best_type == 'spectral':
-                spectral_clustring(temp_dataset, temp_classifier, 40, self.class_names, best_save_path, stage_id, prv_n_class-self.opt.step_num_class)
+                spectral_clustring_mod(temp_dataset, temp_classifier, 40, self.class_names, best_save_path, stage_id, prv_n_class-self.opt.step_num_class)
 
 
             print("\n")
